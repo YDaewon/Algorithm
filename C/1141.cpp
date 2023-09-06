3db01a4906ab4070223a399e244bd8cca902a76a
YDaewon <qkqh9779@gmail.com>
2023-09-06 19:05:09 +0900

    1141 풀이 완료
    처음엔 각 문자열의 조합을 이용하여 풀었는데, 메모리 초과가 나와 그냥 한번에 연산했다

---
+++ C/1141.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<string> s;
+    int n;
+    vector<bool> ers;
+    cin >> n;
+    string t;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        s.push_back(t);
+        ers.push_back(false);
+    }
+    sort(s.begin(), s.end());
+    for (int i = 0; i < s.size();i++) {
+        string a, b;
+        for (int j = i + 1; j < s.size();j++) {
+            a = s[i].substr(0, s[i].size());
+            b = s[j].substr(0, s[i].size());
+            if (a == b) {
+                ers[i] = true;
+                break;
+            }
+        }
+    }
+    int cnt = 0;
+    for (bool i : ers) {
+        if (!i) cnt++;
+    }
+    cout << cnt;
+}
