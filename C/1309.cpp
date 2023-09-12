825c92baeb5bced3cfe365f8851bdf749d5e983f
YDaewon <qkqh9779@gmail.com>
2023-09-13 00:35:41 +0900

    1303, 1309 풀이완료

---
+++ C/1309.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> zz = { 1,1 };
+vector<int> zo = { 0,1 };
+vector<int> oz = { 0,1 };
+
+void dp(int n) {
+    for (int i = 2;i <= n + 1;i++) {
+        int t = i - 1;
+        zz.push_back((zz[t] + zo[t] + oz[t]) % 9901);
+        oz.push_back((zz[t] + zo[t]) % 9901);
+        zo.push_back((zz[t] + oz[t]) % 9901);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    dp(n);
+    cout << zz[n + 1];
+}
