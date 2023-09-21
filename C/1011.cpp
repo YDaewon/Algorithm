c30217c80383c0633b043ec74a9106d14c5418c1
YDaewon <qkqh9779@gmail.com>
2023-09-21 21:14:15 +0900

    1011, 1629, 1660 풀이 완료
    분할 정복에 대해 알아볼 수 있었고, 이제 시간이 남으면 종종 골드 문제도 풀어보려고한다

---
+++ C/1011.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int s, e;
+
+int sol() {
+    int d = e - s;
+    int r = sqrt(d);
+    int temp = r * (r + 1);
+    int ans = 2 * r;
+    if (pow(r, 2) == d) ans -= 1;
+    else if (d > temp && pow(r, 2) < d) ans += 1;
+    return ans;
+}
+
+int main() {
+    int c;
+    cin >> c;
+    while (c--) {
+        cin >> s >> e;
+        cout << sol() << endl;
+    }
+}
