dc21eddb91eb1d3d82d06526385c3013f6d1211b
YDaewon <qkqh9779@gmail.com>
2024-08-19 21:34:50 +0900

    :white_check_mark: 1225, 17281 Solved

---
+++ SWEA/src/Solution_1225.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution_1225 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int T = 1; T < 11; T++){
+            int n = sc.nextInt();
+            Deque<Integer> q = new ArrayDeque<>();
+            for (int i = 0; i < 8; i++) {
+                q.add(sc.nextInt());
+            }
+            int t = 1;
+            int temp;
+            while (true){
+                temp = q.pollFirst() - t;
+                q.addLast(temp >= 0 ? temp : 0);
+                t++;
+                if(t > 5 ) t = 1;
+                if(q.peekLast() <= 0) break;
+            }
+            System.out.print("#" + T + " ");
+            for(int i : q) System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
