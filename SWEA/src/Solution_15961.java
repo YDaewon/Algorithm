2a1770f99ce4630175578264ae6763a939e79f5f
YDaewon <qkqh9779@gmail.com>
2024-09-03 21:04:17 +0900

    :white_check_mark: 240903 Solved

---
+++ SWEA/src/Solution_15961.java
@@ -0,0 +1,43 @@
+package SWEA;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Solution_15961 {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n = sc.nextInt(); // 접시의 수
+		int d = sc.nextInt(); // 초밥 종류
+		int k = sc.nextInt(); // 접시 수
+		int c = sc.nextInt(); // 쿠폰 번호
+		Deque<Integer> dq = new ArrayDeque<>();
+		int [] arr = new int [n];
+		int [] check = new int[d+1];
+
+		for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+		int ans = ++check[c];
+		for(int i = 0; i < k; i++){
+			if(check[arr[i]] == 0) ans++;
+			check[arr[i]]++;
+		}
+
+		int cnt = ans;
+		for(int i = 1; i <= n; i++) {
+			int pop = arr[i-1];
+			check[pop]--;
+			if(check[pop] == 0) {
+				cnt--;
+			}
+
+			int push = arr[(i+k-1) % n];
+			if(check[push] == 0) cnt++;
+			check[push]++;
+
+			ans = Math.max(cnt, ans);
+		}
+		System.out.println(ans);
+	}
+}
---
+++ SWEA/src/Solution_4485.java
@@ -0,0 +1,67 @@
+package SWEA;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Solution_4485 {
+	static int [] dy = {-1,1,0,0};
+	static int [] dx = {0,0,-1,1};
+	static Scanner sc =new Scanner(System.in);
+	static int n = 1, DoongDDangE;
+	static int [][] map;
+	static boolean [][] visit;
+	public static void main(String[] args) {
+		int t= 1;
+		while(true) {
+			n = sc.nextInt();
+			if(n == 0) break;
+			init();
+			sol();
+			System.out.println("Problem " + t + ": " + DoongDDangE);
+			t++;
+		}
+	}
+	
+	static void init() {
+		DoongDDangE = Integer.MAX_VALUE;
+		map = new int [n][n];
+		visit = new boolean[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+	}
+	
+	static void sol() {
+		PriorityQueue<Point> Fat = new PriorityQueue<>();
+		Fat.offer(new Point(0,0,map[0][0]));
+		visit[0][0] = true;
+		while(!Fat.isEmpty()) {
+			Point p = Fat.poll();
+			if(p.y == n-1 && p.x == n-1) DoongDDangE = Math.min(DoongDDangE, p.cost);
+			for(int i = 0; i < 4; i++) {
+				int ny = p.y + dy[i];
+				int nx = p.x + dx[i];
+				if(ny > n-1 || ny < 0 || nx > n-1 || nx < 0) continue;
+				if(!visit[ny][nx]) {
+					Fat.offer(new Point(ny,nx, p.cost+ map[ny][nx]));
+					visit[ny][nx]= true;
+				}
+			}
+		}
+	}
+	
+	static class Point implements Comparable<Point>{
+		int y, x, cost;
+		Point(int y, int x, int cost){
+			this.y = y;
+			this.x = x;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Point o) {
+			return this.cost - o.cost;
+		}
+	}
+}
