ed6f154f74fac9ecca42cc7fbebc686688026236
양대원 <qkqh9860@naver.com>
2024-09-22 21:33:06 +0900

    :white_check_mark: Solved

---
+++ Java/src/Solution_2003.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution_2003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [] arr = new int [n+1];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int f = 0;
+        int e = 0;
+        int sum = 0;
+        int ans = 0;
+        while(e <= n){
+            if(sum < m) sum += arr[e++];
+            else sum -= arr[f++];
+
+            if(sum == m) ans++;
+        }
+        System.out.println(ans);
+    }
+}
---
+++ Java/src/Solution_2531.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution_2531 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int d = sc.nextInt();
+        int k = sc.nextInt();
+        int c = sc.nextInt();
+        int [] sushi = new int [N + k];
+        for (int i = 0; i < N; i++) {
+            int t = sc.nextInt();
+            if(i < k) sushi[N+i] = t;
+            sushi[i] = t;
+        }
+
+        int ans = 0;
+        boolean [] visit = new boolean[d+1];
+        for (int left = 0; left < N; left++) {
+            int cnt = 0;
+            for(int right = left; right < left+k; right++){
+               if (!visit[sushi[right]]) {
+                   visit[sushi[right]] = true;
+                   cnt++;
+               }
+            }
+            if(!visit[c]) cnt++;
+            ans = Math.max(ans, cnt);
+            Arrays.fill(visit, false);
+        }
+
+        System.out.println(ans);
+    }
+}
