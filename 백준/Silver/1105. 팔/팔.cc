402d04d35eabfc2e5153e0cd6d7d55ad96740bd7
YDaewon <138689677+YDaewon@users.noreply.github.com>
2024-10-10 00:26:16 +0900

    [Silver I] Title: 팔, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string l,r;
+    cin >> l >> r;
+    while(l.size() != r.size()){
+        if(l.size() < r.size())
+            l.insert(0,"0");
+        else
+            r.insert(0,"0");
+    }
+    int count = 0;
+    for(int i = 0; i < l.size();i++){
+        if(l[i] == r[i]){
+            if (l[i] == '8')
+                count++;
+        }
+        else
+            break;
+    }
+    cout << count;
+}
