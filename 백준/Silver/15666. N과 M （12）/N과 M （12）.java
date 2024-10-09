dcd4d6aafb67e4c44c49ad109a273391ddeefa10
YDaewon <138689677+YDaewon@users.noreply.github.com>
2024-10-10 01:07:27 +0900

    [Silver II] Title: N과 M (12), Time: 408 ms, Memory: 21472 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int m;
+    static int [] arr;
+    static List<Integer> ans;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        ans = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        sol(0,0);
+    }
+
+    static void sol(int depth, int now){
+        if(depth == m){
+            for (int i = m-1; i >= 0; i--) {
+                System.out.print(ans.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+        int temp = 0;
+        for (int i = now; i < n; i++) {
+            if(arr[i] != temp) {
+                temp = arr[i];
+                ans.add(0, arr[i]);
+                sol(depth + 1, i);
+                ans.remove(0);
+            }
+        }
+    }
+}
