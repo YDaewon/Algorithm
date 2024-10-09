66d4bdb0b9a627e30a892712b808540e55aafe46
YDaewon <138689677+YDaewon@users.noreply.github.com>
2024-10-10 01:06:52 +0900

    [Silver III] Title: N과 M (2), Time: 232 ms, Memory: 18364 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        combination(0, 1, n, m);
+    }
+    static int [] visit = new int[9];
+    static void combination(int depth, int now, int n, int m){
+        if(depth == m){
+            for (int i = 1; i <= n; i++) {
+                if(visit[i] == 1){
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = now; i <= n; i++) {
+            if (visit[i] == 0) {
+                visit[i] = 1;
+                combination(depth + 1, i, n, m);
+                visit[i] = 0;
+            }
+        }
+    }
+}
