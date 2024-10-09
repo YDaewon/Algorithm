f72dacc8b2f3c2521eaa35aea0db015880aad3ff
YDaewon <138689677+YDaewon@users.noreply.github.com>
2024-10-10 00:17:19 +0900

    [Gold V] Title: 물병, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main(){
+    int n,k,count=0;
+    cin >> n >> k;
+    bitset<32> b_n(n);
+    while(b_n.count() > k){
+        count++;
+        b_n = n + count;
+    }
+   cout << count;       
+}
