9ec5cfe7b7c4e39594b1a99d29019bb5f3178909
YDaewon <138689677+YDaewon@users.noreply.github.com>
2024-11-06 11:32:40 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 252 ms, Memory: 18432 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,49 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
-	static int n, ans = 0;
-	static int [][] egg;
+	static Egg [] egg;
+	static int n, ans;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		egg = new int [n][2]; // 0: 내구도 , 1: 무게
-		for(int i = 0; i < n;i++) {
-			egg[i][0] = sc.nextInt();
-			egg[i][1] = sc.nextInt();
+		sc.nextLine();
+		egg = new Egg [n];
+		ans = 0;
+		for(int i = 0; i < n; i++) {
+			int s = sc.nextInt();
+			int w = sc.nextInt();
+			egg[i] = new Egg(s,w);
 		}
-		sol(0, 0);
+		sol(0,0);
 		System.out.println(ans);
 	}
-	
-	static void sol(int now, int cnt) {
-		//System.out.println(now);
+	private static void sol(int now, int cnt) {
 		if(now == n) {
 			ans = Math.max(ans, cnt);
 			return;
 		}
-		if(egg[now][0] <= 0 || cnt == n-1) {
+		if(egg[now].s <= 0 || cnt >= n-1) {
 			sol(now+1, cnt);
 			return;
 		}
-		int temp = cnt;
-		for(int i = 0; i < n;i++) {
+		
+		int t = cnt;
+		for(int i = 0; i < n; i++) {
 			if(i == now) continue;
-			if(egg[i][0] <= 0) continue;
-			crack(now, i);
-			if(egg[now][0] <= 0) cnt++;
-			if(egg[i][0] <= 0) cnt++;
+			if(egg[i].s <= 0) continue;
+			Break(i, now);
+			if(egg[i].s <= 0) cnt++;
+			if(egg[now].s <= 0) cnt++;
 			sol(now+1, cnt);
-			btb(now,i);
-			cnt = temp;
+			rollback(i, now);
+			cnt = t;
 		}
 	}
 	
-	static void crack(int a, int b) {
-		egg[a][0] -= egg[b][1];
-		egg[b][0] -= egg[a][1];
+	static void Break(int a, int b) {
+		egg[a].s -= egg[b].w;
+		egg[b].s -= egg[a].w;
 	}
-	static void btb(int a, int b) {
-		egg[a][0] += egg[b][1];
-		egg[b][0] += egg[a][1];
+	static void rollback(int a, int b) {
+		egg[a].s += egg[b].w;
+		egg[b].s += egg[a].w;
+	}
+	
+	
+	static class Egg{
+		int s, w;
+		Egg(int s, int w){
+			this.s = s;
+			this.w = w;
+		}
 	}
 }
