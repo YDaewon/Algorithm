84e087c8f7a131b7bdd002a4948a8ee5a45d61bb
YDaewon <138689677+YDaewon@users.noreply.github.com>
2024-12-09 21:55:57 +0900

    [Gold III] Title: 게임 개발, Time: 240 ms, Memory: 21496 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,47 +1,87 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static List<Integer> [] build;
-    static int n;
-    static int [] time, result, indegree;
-    static Queue<Integer> q;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        build = new ArrayList[n+1];
-        indegree = new int [n+1];
-        result = new int [n+1];
-        q = new LinkedList<>();
-        for(int i = 1; i <= n; i++) build[i] = new ArrayList<>();
-        time = new int [n+1];
-        for(int i = 1; i <= n; i++){
-            time[i] = sc.nextInt();
-            int t = sc.nextInt();
-            while (t != -1){
-                build[t].add(i);
-                indegree[i]++;
-                t = sc.nextInt();
+    static int N;
+    static int[] time;
+    static int[] preCnt;
+    static int[] res;
+    static ArrayList<Integer>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 건물 종류의 수
+        time = new int[N+1]; // 각 건물을 짓는데 걸리는 시간을 담아줄 배열
+        preCnt = new int[N+1]; // 각 건물 별 먼저 지어야 할 건물의 수
+
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        int idx = 1;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int flag = 0;
+
+            while (true) {
+                int value = Integer.parseInt(st.nextToken());
+                if (value == -1) break;
+
+                if (flag == 0) {
+                    time[idx] = value;
+                } else {
+                    graph[value].add(idx);
+                    preCnt[idx]++;
+                }
+
+                flag++;
+            }
+            idx++;
+        }
+
+        res = new int[N+1];
+        Build();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(res[i]);
+        }
+    }
+
+    // time = {0, 10, 20, 14, 4, 3}
+    // preCnt = {0, 0, 1, 1, 2, 1}
+    private static void Build() {
+        Queue<Integer> q = new LinkedList<>();
+
+        // 큐에 먼저 지어져야하는 건물이 없는 건물만 삽입
+        for (int i = 1; i <= N; i++) {
+            if(preCnt[i] == 0) {
+                q.offer(i); // {1, 10}
+                res[i] = time[i];
             }
         }
-        //for(int i : indegree) System.out.println(i);
-        for(int i = 1; i <= n; i++){
-            if(indegree[i] == 0) {
-                result[i] = time[i];
-                q.add(i);
-            }
-        }
+
         while(!q.isEmpty()) {
             int cur = q.poll();
-           // System.out.println("now idx: " + cur);
-            for (int i = 0; i < build[cur].size(); i++) {
-                int next = build[cur].get(i);
-                result[next] = Math.max(result[next], result[cur] + time[next]);
-                indegree[next]--;
-                if(indegree[next] == 0) q.add(next);
+
+            for(int b : graph[cur]) {
+                res[b] = Math.max(res[b], res[cur] + time[b]);
+                preCnt[b]--;
+                if(preCnt[b] == 0) q.offer(b);
             }
         }
-
-
-        for(int i =1;i <= n;i++) System.out.println(result[i]);
     }
 }
+
+// graph = [[],
+//          [2, 3, 4],
+//          [],
+//          [4, 5],
+//          [],
+//          []]
