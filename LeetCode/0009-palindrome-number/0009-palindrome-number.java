8fa5fd4c7917b2f288050befaa6c2b7266a25dd2
YDaewon <138689677+YDaewon@users.noreply.github.com>
2025-06-04 09:32:06 +0900

    Time: 5 ms (69.87%), Space: 44 MB (76.78%) - LeetHub

--- LeetCode/0009-palindrome-number/0009-palindrome-number.java
+++ LeetCode/0009-palindrome-number/0009-palindrome-number.java
@@ -3,12 +3,12 @@ class Solution {
         if(x < 0) return false;
         int s = x;
         int r = 0;
-        while(s != 0){
-            int t = s % 10;
-            s /= 10;
+        while(x != 0){
+            int t = x % 10;
+            x /= 10;
             r = r*10 + t;
         }
         //System.out.println(x + ", " + r);
-        return x == r;
+        return s == r;
     }
 }
