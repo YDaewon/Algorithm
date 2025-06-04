6a68860a41ae53739fc9cf705ad3f487559a9763
YDaewon <138689677+YDaewon@users.noreply.github.com>
2025-06-04 09:30:42 +0900

    Time: 5 ms (69.87%), Space: 44.2 MB (38.15%) - LeetHub

--- LeetCode/0009-palindrome-number/0009-palindrome-number.java
+++ LeetCode/0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) return false;
-        char [] num = String.valueOf(x).toCharArray();
-        int len = num.length;
-        int mid = len / 2;
-        for(int i = 0; i < mid; i++){
-            if(num[i] != num[len - 1 - i]) return false;
+        int s = x;
+        int r = 0;
+        int t = 0;
+        while(s != 0){
+            t = s % 10;
+            s /= 10;
+            r = r*10 + t;
         }
-        return true;
+        //System.out.println(x + ", " + r);
+        return x == r;
     }
 }
