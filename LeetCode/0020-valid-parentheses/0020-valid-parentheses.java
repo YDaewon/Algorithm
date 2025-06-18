c08927fc0d722ee5d6fa253cdffbd0fa2bc196c3
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-06-18 16:31:36 +0900

    Time: 2 ms (97.1%), Space: 41.8 MB (65.58%) - LeetHub

--- LeetCode/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,20 +2,23 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < s.length(); i++){
+        for(char c : s.toCharArray()){
             if(stack.isEmpty()){
-                stack.add(s.charAt(i));
+                stack.push(c);
             }
             else{
-                if(stack.peek() == '(' && s.charAt(i) == ')'){
+                if(c == '(' || c == '[' || c == '{' ){
+                    stack.push(c);
+                }
+                else if(stack.peek() == '(' && c == ')'){
                     stack.pop();
-                } else if(stack.peek() == '{' && s.charAt(i) == '}'){
+                } else if(stack.peek() == '{' && c == '}'){
                     stack.pop();
-                } else if(stack.peek() == '[' && s.charAt(i) == ']'){
+                } else if(stack.peek() == '[' && c == ']'){
                     stack.pop();
                 }
                 else{
-                    stack.add(s.charAt(i));
+                    return false;
                 }
             }
         }
