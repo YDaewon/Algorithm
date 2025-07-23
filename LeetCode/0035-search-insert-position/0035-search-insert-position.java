b48126efc19a763ab08ec6539b7120cf23a85564
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-07-23 12:01:17 +0900

    Time: 0 ms (100%), Space: 42.9 MB (65.98%) - LeetHub

--- LeetCode/0035-search-insert-position/0035-search-insert-position.java
+++ LeetCode/0035-search-insert-position/0035-search-insert-position.java
@@ -2,6 +2,6 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int val = Arrays.binarySearch(nums, target);
         if(val >= 0) return val;
-        else return Math.abs(val) -1;
+        else return -1*val -1;
     }
 }
