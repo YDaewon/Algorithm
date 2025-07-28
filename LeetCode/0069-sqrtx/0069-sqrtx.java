c4d331ad6a1ed2c8f78c8db2c22b1ab76302d5e4
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-07-28 12:55:11 +0900

    Time: 1 ms (84.67%), Space: 41.1 MB (40.83%) - LeetHub

--- LeetCode/0069-sqrtx/0069-sqrtx.java
+++ LeetCode/0069-sqrtx/0069-sqrtx.java
@@ -1,9 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        int cnt = 0;
-        while((cnt+1)*(cnt+1) <= x){
-            cnt++;
+        //return (int)Math.floor(Math.sqrt(x));
+
+        if (x < 2) return x;
+
+        int left = 2;
+        int right = x;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            long temp = (long)mid * mid;
+            if(temp < x) left = mid + 1;
+            else if (temp > x) right = mid - 1;
+            else return mid;
         }
-        return cnt;
+
+        return right;
     }
 }
