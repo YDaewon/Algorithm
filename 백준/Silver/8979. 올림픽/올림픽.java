260fe98162098213b72e74351a36ef76f7f53adc
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-09-20 19:23:55 +0900

    [Silver V] Title: 올림픽, Time: 100 ms, Memory: 14048 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int [] answer = new int [4];
+
+        List<int []> nums = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            int [] country = new int [4];
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 4; j++) {
+                country[j] = Integer.parseInt(st.nextToken());
+                if (k == country[j]) answer[j] = country[j];
+            }
+            nums.add(country);
+        }
+
+        Collections.sort(nums, (n1, n2) -> {
+            if(n1[1] != n2[1]) return n1[1] - n2[1];
+            if(n1[2] != n2[2]) return n1[2] - n2[2];
+            if(n1[3] != n2[3]) return n1[3] - n2[3];
+            return 0;
+        });
+
+        int cnt = 0;
+        for(int i = 1; i < n; i++){
+            int [] bef = nums.get(i - 1);
+            int [] cur = nums.get(i);
+            if(bef[1] != cur[1] || bef[2] != cur[2] || bef[3] != cur[3]){
+                cnt++;
+            }
+            if(bef[0] == k){
+                System.out.println(cnt);
+                break;
+            }
+        }
+    }
+}
