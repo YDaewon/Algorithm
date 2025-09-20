505cd33069218c8fffd8b3011739ef9596a4d9d5
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-09-20 19:34:33 +0900

    [Silver V] Title: 올림픽, Time: 132 ms, Memory: 14700 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -22,23 +22,28 @@ public class Main {
         }
 
         Collections.sort(nums, (n1, n2) -> {
-            if(n1[1] != n2[1]) return n1[1] - n2[1];
-            if(n1[2] != n2[2]) return n1[2] - n2[2];
-            if(n1[3] != n2[3]) return n1[3] - n2[3];
+            if(n1[1] != n2[1]) return n2[1] - n1[1];
+            if(n1[2] != n2[2]) return n2[2] - n1[2];
+            if(n1[3] != n2[3]) return n2[3] - n1[3];
             return 0;
         });
 
-        int cnt = 0;
+        if(nums.get(0)[0] == k){
+            System.out.println(1);
+            return;
+        }
+
+        int cnt = 1;
         for(int i = 1; i < n; i++){
             int [] bef = nums.get(i - 1);
             int [] cur = nums.get(i);
             if(bef[1] != cur[1] || bef[2] != cur[2] || bef[3] != cur[3]){
-                cnt++;
+                cnt = i + 1;
             }
-            if(bef[0] == k){
-                System.out.println(cnt);
+            if(cur[0] == k){
                 break;
             }
         }
+        System.out.println(cnt);
     }
 }
