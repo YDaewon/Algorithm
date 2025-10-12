16ef9978571be8b488b56e73346b9ccc0c159622
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-10-12 18:03:21 +0900

    Time: 0 ms (100%), Space: 41.4 MB (10.77%) - LeetHub

--- LeetCode/0190-reverse-bits/0190-reverse-bits.java
+++ LeetCode/0190-reverse-bits/0190-reverse-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int res = 0;
         for(int i = 0; i < 32; i++){
             int t = (n & (1 << i)) == 0 ? 0 : 1;
-            System.out.print(t);
+            //System.out.print(t);
             if(t != 0) res |= (1 << 31 - i);
         }
         return res;
