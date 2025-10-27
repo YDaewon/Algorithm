02bff430378d5241b8e02be5d8383de8bafefe9b
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-10-27 15:34:19 +0900

    Time: 13 ms (87.75%), Space: 58.2 MB (71.63%) - LeetHub

--- LeetCode/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,20 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // for(int i : nums){
+        //     if(map.getOrDefault(i, 0) == 0) map.put(i, 1);
+        //     else return true;
+        // }
+
+        // return false;
+
+        Set<Integer> set = new HashSet<>();
 
         for(int i : nums){
-            if(map.getOrDefault(i, 0) == 0) map.put(i, 1);
-            else return true;
+            if(set.contains(i)) return true;
+            set.add(i);
         }
-
         return false;
     }
 }
