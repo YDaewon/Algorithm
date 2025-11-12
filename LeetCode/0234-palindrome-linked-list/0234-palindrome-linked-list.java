b29e5cd11ecd56164aec25c6653f314eab31a3a6
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-11-12 12:36:17 +0900

    Time: 3 ms (99.75%), Space: 94.6 MB (7.28%) - LeetHub

--- LeetCode/0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ LeetCode/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,26 +9,58 @@
  * }
  */
 class Solution {
+    // public boolean isPalindrome(ListNode head) {
+    //     Stack<Integer> s = new Stack<>();
+    //     Stack<Integer> s2 = new Stack<>();
+    //     int len = 0;
+
+    //     ListNode temp = head;
+    //     while(temp != null){
+    //         s.add(temp.val);
+    //         temp = temp.next;
+    //         len++;
+    //     }
+
+    //     for(int i = 0; i < len/2; i++){
+    //         s2.add(s.pop());
+    //     }
+    //     if(len % 2 == 1) s.pop();
+    //     while(!s.isEmpty() && !s2.isEmpty()){
+    //         if(s.pop() != s2.pop()) return false;
+    //     }
+    //     return true;
+
+    // }
+
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> s = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
-        int len = 0;
+        ListNode fast = head;
+        ListNode slow = head;
 
-        ListNode temp = head;
-        while(temp != null){
-            s.add(temp.val);
-            temp = temp.next;
-            len++;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
 
-        for(int i = 0; i < len/2; i++){
-            s2.add(s.pop());
+        if(fast != null) slow = slow.next; // odd
+        
+        ListNode start = head;
+
+        ListNode cur = slow;
+        ListNode pre = null;
+
+        while(cur != null){
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
         }
-        if(len % 2 == 1) s.pop();
-        while(!s.isEmpty() && !s2.isEmpty()){
-            if(s.pop() != s2.pop()) return false;
+
+        while(pre != null){
+            if(start.val != pre.val) return false;
+            start = start.next;
+            pre = pre.next;
         }
+
         return true;
-
     }
 }
