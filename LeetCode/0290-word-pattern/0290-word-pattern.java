844450016cc8c6d1263993c63ec650215adff0a3
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-12-02 14:26:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/0290-word-pattern/0290-word-pattern.java
+++ LeetCode/0290-word-pattern/0290-word-pattern.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String [] str = s.split(" ");
-        Map<String, Character> sac = new HashMap<>();
-        Map<Character, String> cas = new HashMap<>();
-        if(str.length != pattern.length()) return false;
+        Map<Character, String> pat = new HashMap<>();
+
         for(int i = 0; i < str.length; i++){
-            char c = pattern.charAt(i);
-            if(cas.get(c) == null){
-                if(sac.containsKey(str[i])) return false;
-                cas.put(c, str[i]);
-                sac.put(str[i], c);
+            if(pat.getOrDefault(pattern.charAt(i), null) == null){
+                pat.put(pattern.charAt(i), str[i]);
             }
             else{
-                if(!cas.get(c).equals(str[i])) return false;
+                if(!pat.get(pattern.charAt(i)).equals(str[i])) return false;
             }
         }
         return true;
