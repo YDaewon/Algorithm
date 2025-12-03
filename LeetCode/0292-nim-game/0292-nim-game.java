05b09a6fd5e4c2eabf7f5736531115fb7c27bb59
Daebok <138689677+YDaewon@users.noreply.github.com>
2025-12-03 18:08:02 +0900

    Time: 1 ms (0.38%), Space: 42.2 MB (8.77%) - LeetHub

---
+++ LeetCode/0292-nim-game/0292-nim-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        int cnt = 0;
+        while(n % 4 != 0){
+            cnt++;
+            n--;
+        }
+        System.out.println(cnt);
+
+        if(cnt == 0)return false;
+        else return true;
+    }
+}
