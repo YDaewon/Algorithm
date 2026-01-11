7668693d9734082bf586c68a9c5c67cf915113a3
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-01-11 23:07:23 +0900

    [Silver II] Title: KCPC, Time: 292 ms, Memory: 35040 KB -BaekjoonHub

---
+++ 백준/Silver/3758. KCPC/KCPC.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken()); // 팀 개수
+            int k = Integer.parseInt(st.nextToken()); // 문제 개수
+            int my_id = Integer.parseInt(st.nextToken()) - 1; // 내 ID
+            int m = Integer.parseInt(st.nextToken()); // 로그 엔트리 개수
+
+            Team [] team = new Team[n];
+            for (int i = 0; i < n; i++) team[i] = new Team(i, k);
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int tid = Integer.parseInt(st.nextToken()) - 1;
+                int pid = Integer.parseInt(st.nextToken()) - 1;
+                int score = Integer.parseInt(st.nextToken());
+
+                team[tid].addLog(pid, score, i);
+            }
+
+            for (int i = 0; i < n; i++) {
+                team[i].getTotel();
+                // System.out.println("team[" + (team[i].teamId+1) + "]'s score: " + team[i].total);
+            }
+
+            Arrays.sort(team);
+
+           // for (int i = 0; i < n; i++) {
+               // System.out.println("team[" + (team[i].teamId + 1) + "]'s rank: " + (i+1));
+           // }
+
+            for(int i = 0; i < n; i++){
+                if(team[i].teamId == my_id) {
+                    System.out.println(i + 1);
+                    break;
+                }
+            }
+        }
+    }
+
+}
+
+class Team implements Comparable<Team>{
+    int teamId;
+    int submitCount;
+    int [] problem;
+    int submitTime;
+    int total;
+
+    @Override
+    public int compareTo(Team o){
+        if(this.total == o.total && this.submitCount == o.submitCount) return this.submitTime > o.submitTime ? 1 : -1;
+        else if( this.total == o.total) return this.submitCount > o.submitCount ? 1 : -1;
+        else return this.total < o.total ? 1 : -1;
+    }
+
+    public Team(int id, int p_count){
+        this.teamId = id;
+        this.submitCount = 0;
+        this.problem = new int[p_count];
+        this.submitTime = 0;
+    }
+
+    public void addLog(int pid, int score, int time){
+        submitCount++;
+        this.submitTime = time;
+        problem[pid] = Math.max(problem[pid], score);
+    }
+
+    public void getTotel(){
+        for (int i : problem) {
+            total += i;
+        }
+    }
+}
