ed6a1ccef5877044fd223fcd10667eb47d23d8d2
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-01-22 18:39:30 +0900

    [Silver III] Title: 주유소, Time: 380 ms, Memory: 35588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -7,7 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int [] road = new int [n-1];
+        int [] road = new int [n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n-1; i++) {
             road[i] = Integer.parseInt(st.nextToken());
@@ -19,26 +19,14 @@ public class Main {
             city[i] = Integer.parseInt(st.nextToken());
         }
 
-        long sum = 0;
         int price = city[0];
+        long sum = (long) price * road[0];
         for (int i = 1; i < n; i++) {
             if(price > city[i]){
-                sum += (long) price * road[i-1];
+                price = city[i];
             }
-            else{
-                int j = i;
-                int temp = 0;
-                while(price < city[j]){
-                    temp += road[j-1];
-                    j++;
-                }
-                temp += road[j-1];
-                // System.out.println("구간: " + (i-1) + " ~ " + j + " 합: " + temp);
-                sum += (long) temp * price;
-                i = j;
-            }
-            // System.out.println("now: " + i + ", sum: " + sum);
-            price = city[i];
+            sum += (long) price * road[i];
+            // System.out.println("now sum: " + sum);
         }
 
         System.out.println(sum);
