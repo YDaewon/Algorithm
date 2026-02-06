5db932978b76e0e0c3b685f99246049298416d3a
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-06 14:50:20 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 160 ms, Memory: 19768 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -1,35 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 
 public class Main {
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st;
-
         Stack<Integer> s = new Stack<>();
-        int cnt = 0;
-
+        int answer = 0;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            while(!s.isEmpty() && s.peek() > y){
-                if(s.peek() != 0) cnt++;
+            while (!s.isEmpty() && s.peek() > y) {
                 s.pop();
-                // System.out.println("cnt++");
+                answer++;
             }
-            if(s.isEmpty() || s.peek() != y) s.push(y);
-            // System.out.println("stack size: " + s.size() + ", now top: " + (s.isEmpty() ? "x" : s.peek()));
+            if(y == 0) continue;
+            if(!s.isEmpty() && s.peek() == y) continue;
+            s.add(y);
         }
-        while(!s.isEmpty()){
-            if(s.peek() != 0) cnt++;
-            s.pop();
-        }
-        System.out.println(cnt);
+        System.out.println(answer + s.size());
+
     }
 
 }
