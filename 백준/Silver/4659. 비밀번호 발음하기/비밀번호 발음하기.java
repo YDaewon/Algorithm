cff4c1fee030ee667eb47c4840a6945512bb7a18
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-09 10:20:17 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 116 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -1,50 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        while(true) {
+        
+        while(true){
             String str = br.readLine();
+            char [] carr = str.toCharArray();
             if(str.equals("end")) break;
             boolean flag1 = false;
             boolean flag2 = true;
             boolean flag3 = true;
-            int v_cnt = 0;
-            int c_cnt = 0;
-            char bef = '1';
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-                if(bef == c && (c != 'e' && c != 'o')) {
-                    flag3 = false;
-                    break;
-                }
-                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            
+            int v = 0;
+            int c = 0;
+
+            for (int i = 0; i < carr.length; i++) {
+                if(carr[i] == 'a' || carr[i] =='e' || carr[i] == 'i' || carr[i] == 'o' || carr[i] =='u'){
                     flag1 = true;
-                    if(v_cnt >= 2) {
-                        flag2 = false;
-                        break;
-                    }
-                    c_cnt = 0;
-                    v_cnt++;
+                    c++;
+                    v = 0;
+                }else{
+                    c = 0;
+                    v++;
                 }
-                else{
-                    if(c_cnt >= 2) {
-                        flag2 = false;
-                        break;
-                    }
-                    v_cnt = 0;
-                    c_cnt++;
+                if(v >= 3 || c >= 3) flag2 = false;
+
+                if(i > 0){
+                    if(carr[i-1] == carr[i] && carr[i] != 'e' && carr[i] != 'o') flag3 = false;
                 }
-                bef = c;
             }
-            if(flag1 && flag2 && flag3) sb.append("<"+str+"> is acceptable.\n");
-            else sb.append("<"+str+"> is not acceptable.\n");
+
+
+            if(flag1 && flag2 && flag3) System.out.println("<" + str + "> is acceptable.");
+            else System.out.println("<" + str + "> is not acceptable.");
         }
-        System.out.println(sb);
     }
 
 }
