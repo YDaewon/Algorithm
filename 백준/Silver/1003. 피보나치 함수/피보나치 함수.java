244ab54b8b244f4ccb2b7cd61fe1c56cca075aa2
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-14 15:12:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 15984 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,30 @@
+ import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int [] zdp = new int [41];
+    static int [] odp = new int [41];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        zdp[0] = 1;
+        zdp[1] = 0;
+        odp[0] = 0;
+        odp[1] = 1;
+
+        for(int i = 2; i <= 40; i++){
+            zdp[i] = zdp[i-1] + zdp[i-2];
+            odp[i] = odp[i-1] + odp[i-2];
+        }
+        StringBuilder sb = new StringBuilder();
+        while(T-- > 0){
+            int n = Integer.parseInt(br.readLine());
+            sb.append(zdp[n] + " " + odp[n] + "\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    
+}
