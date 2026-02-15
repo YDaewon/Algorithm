81d34521f0966a50bf3d28ea30a0a3501e50547a
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-15 15:11:30 +0900

    [Gold IV] Title: 치즈, Time: 128 ms, Memory: 16444 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,76 +1,74 @@
+ import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int [][]board;
-	static int n, m, cnt, before, ans;
-	
-	static int [] dy = {-1,1,0,0};
-	static int [] dx = {0,0,-1,1};
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		m = sc.nextInt();
-		board = new int[n][m];
-		cnt = before = ans = 0;
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				board[i][j] = sc.nextInt();
-				if(board[i][j] == 1) {
-					cnt++;
-				}
-			}
-		}
-		int time = 0;
-		while(cnt != 0) {
-			bfs();
-			time++;
-		}
-		
-		System.out.println(time);
-		System.out.println(before);
-	}
-	
-	static void bfs() {
-		before = cnt;
-		Queue<Pair> q = new LinkedList<>();
-		int [][] visit = new int [n][m];
-		q.add(new Pair(0,0));
-		visit[0][0] = 1;
-		while(!q.isEmpty()) {
-			Pair p = q.poll();
-			for(int i = 0; i < 4; i++) {
-				int ny = p.y + dy[i];
-				int nx = p.x + dx[i];
-				if(ny >=n || ny < 0 || nx >=m || nx < 0) continue;
-				if(visit[ny][nx] == 0) {
-					if(board[ny][nx] == 0) {
-						q.add(new Pair(ny,nx));
-						visit[ny][nx] = 1;
-					}
-					else if(board[ny][nx] == 1) {
-						visit[ny][nx] = 1;
-						board[ny][nx] = -1;
-					}
-				}
-			}
-		}
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				if(board[i][j] == -1) {
-					board[i][j] = 0;
-					cnt--;
-				}
-			}
-		}
-	}
-	
-	static class Pair{
-		int y,x;
-		Pair(int y, int x){
-			this.y = y;
-			this.x = x;
-		}
-	}
+    static int [] dy = {-1,1,0,0};
+    static int [] dx = {0,0,-1,1};
+
+    static int n, m, cnt = 0;
+    static int [][] chz;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        chz = new int [n][m];
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                chz[i][j] = Integer.parseInt(st.nextToken());
+                if(chz[i][j] == 1) cnt++;
+            }
+        }
+
+        int ans = 0;
+        int bef = 0;
+        while(cnt > 0){
+            bef = cnt;
+            boolean [][] visit = new boolean[n][m];
+            bfs(visit); 
+            ans++;
+        }
+        System.out.println(ans);
+        System.out.println(bef);
+    }
+
+    static void bfs(boolean [][] visit){
+        Queue<int []> q = new LinkedList<>();
+        q.add(new int [] {0, 0});
+        visit[0][0] = true;
+
+        while(!q.isEmpty()){
+            int [] cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = cur[0] + dy[i];
+                int nx = cur[1] + dx[i];
+
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+                if(!visit[ny][nx]){
+                    visit[ny][nx] = true;
+                    if(chz[ny][nx] == 1) {
+                        chz[ny][nx] = 2;
+                        cnt--;
+                    }
+                    else q.add(new int [] {ny,nx});
+                }
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(chz[i][j] == 2) chz[i][j] = 0;
+            }
+        }
+    }
+
+    
 }
