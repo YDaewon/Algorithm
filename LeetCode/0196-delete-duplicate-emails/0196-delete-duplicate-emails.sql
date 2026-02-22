39a64e9dbd501e2e20ce9a30c24095216d9795b9
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-22 21:58:40 +0900

    Time: 1024 ms (10.36%), Space: 0B (100%) - LeetHub

--- LeetCode/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ LeetCode/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-DELETE p FROM Person p
-JOIN Person pp ON p.email = pp.email
-WHERE p.id > pp.id;
-
+-- # Write your MySQL query statement below
+DELETE p FROM Person p, Person pp 
+WHERE p.email = pp.email AND p.id > pp.id
