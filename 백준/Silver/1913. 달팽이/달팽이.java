1cc8050526b9aab43cf7f3e1fe0faa10d600d91a
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-25 12:43:52 +0900

    [Silver III] Title: 달팽이, Time: 532 ms, Memory: 84964 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,43 +1,55 @@
-import java.util.Scanner;
+ import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		int [] dy = {1,0,-1,0};
-		int [] dx = {0,1,0,-1};
-		
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int [] p = new int [2];
-		int [][] board = new int[n][n];
-		int d = 0;
-		int y = 0, x = 0;
-		for(int i = n*n; i >= 1; i--) {
-			if(y < 0 || y >= n || x < 0|| x >= n || board[y][x] != 0 ) {
-				y -= dy[d];
-				x -= dx[d];
-				d = (d+1)%4;
-				y += dy[d];
-				x += dx[d];
-			}
-			board[y][x] = i;
-			if(m == i) {
-				p[0] = y+1;
-				p[1] = x+1;
-			}
-			y += dy[d];
-			x += dx[d];
-			//System.out.println(i + ": (" + y + ", " + x + ")");
-		}
-		
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0 ; j < n ; j++) {
-				System.out.print(board[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println(p[0] + " " + p[1]);
-	}
-	
+    static int n;
+    static int [][] arr;
+    static int [] dy= {1,0,-1,0};
+    static int [] dx = {0,1,0,-1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        int find = Integer.parseInt(br.readLine());
+
+        int y = 1;
+        int x = 1;
+        int d = 0;
+
+        arr = new int[n+2][n+2];
+
+        for (int i = 0; i < n + 1; i++) {
+            arr[i][0] = -1;
+            arr[i][n+1] = -1;
+            arr[0][i] = -1;
+            arr[n+1][i] = -1;
+        }
+
+
+        int ansy = 0;
+        int ansx = 0;
+
+        for (int i = n * n; i >= 1; i--) {
+            arr[y][x] = i;
+            if(arr[y][x] == find) {
+                ansy = y;
+                ansx = x;
+            }
+
+            //System.out.println("y: " + y + ", x: " + x + ", arr[y][x]: " + arr[y][x]);
+            if(arr[y + dy[d]][x + dx[d]] != 0) d = (d+1) % 4;
+            y += dy[d];
+            x += dx[d];
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+        System.out.println(ansy + " " + ansx);
+    }
+
 }
