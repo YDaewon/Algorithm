525ffd945378bf491ee3765f89dabe1544835ca2
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-02 12:13:25 +0900

    Time: 314 ms (89.95%), Space: 0B (100%) - LeetHub

--- LeetCode/0626-exchange-seats/0626-exchange-seats.sql
+++ LeetCode/0626-exchange-seats/0626-exchange-seats.sql
@@ -1,6 +1,7 @@
+# Write your MySQL query statement below
 SELECT
+    -- *
     s.id,
-    IFNULL(ss.student, s.student) AS student
+    CASE WHEN ss.id IS NULL THEN s.student ELSE ss.student END AS student
 FROM Seat s
-LEFT JOIN Seat ss ON (s.id % 2 = 1 AND s.id +1 = ss.id) OR (s.id % 2 = 0 AND s.id - 1 = ss.id)
-ORDER BY 1 ASC
+LEFT JOIN Seat ss ON (s.id % 2 = 1 AND ss.id = s.id + 1) OR (s.id % 2 = 0 AND ss.id = s.id -1)
