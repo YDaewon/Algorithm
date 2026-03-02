f882e589acb316e8de87d3f5fcd0f58a9ba613e2
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-02 12:00:33 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 208 ms, Memory: 28080 KB -BaekjoonHub

--- 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.java
+++ 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.java
@@ -1,94 +1,97 @@
-import java.io.*;
+ import java.io.*;
 import java.util.*;
-/*
-왼쪽 아래가 1,1
-오른쪽 위가 2k, 2k
-y가 새로, x가 가로
-1부터 시작함
 
-
-
-
-*/
 public class Main {
+    static int k;
 
-    static int k, len, y, x;
-
-    static int [][] g;
-    static StringBuilder sb = new StringBuilder();
     static int num = 1;
+    static int [][] tile;
 
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        k = Integer.parseInt(br.readLine());
-        len = (int) Math.pow(2, k);
-        g = new int[len][len];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        x = Integer.parseInt(st.nextToken())-1;
-        y = Integer.parseInt(st.nextToken())-1;
-        g[y][x] = -1;
-        calc(len, 0, 0);
+        k = Integer.parseInt(st.nextToken());
+        int len = (int) Math.pow(2, k);
+        tile = new int [len][len];
 
-        print_answer();
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken()) - 1;
+        int y = Integer.parseInt(st.nextToken()) - 1;
+
+        tile[y][x] = -1;
+
+        lay_tiles(0, 0, len);
+        pnt(len);
+        System.out.println(sb);
     }
     /*
-    8칸짜리
-    0~3, 4~7
+    4분할 후 구멍이 있는 곳 찾기
+    2 1 
+    3 4
+    로 사분면을 정의할 때 경우의 수는
+    o x   x o   o o   o o
+    o o   o o   x o   o x
 
-      o x
-      o o  q: 1
+    사분면이 있는 쪽은 제외, 나머지를 타일 채우기
+    ex
+    0  0  0  0        0  0  0  0
+    0  0  0  0   ->   0  1  1  0
+    0  0 -1  0        0  1 -1  0
+    0  0  0  0        0  0  0  0
 
-      x o   
-      o o  q: 2
+    이후 각 사분면을 다시 4분할
+    이 때 아까 채워둔 타일이 새로운 구멍이 되는 것 = 나머지 빈곳 타일 채우기
+    1이 구멍이 됨
+    2 사분면을 봤을 때
+    0  0   ->     2  2
+    0  1          2  1
 
-      o o
-      x o  q: 3
 
-      o o
-      o x  q: 4
+    각 사분면이 끝난다면
 
-      2 1
-      3 4
+    3  3  2  2
+    3  1  1  2
+    4  1 -1  5 
+    4  4  5  5
 
+    완성
     */
 
-    
+    static void lay_tiles(int y, int x, int len) {
+        if(len == 1) return;
 
-    static void calc(int now, int y, int x){
-        if(now == 1) return;
+        int now = len/2;
 
-        int quadrant = 0;
-        int h = now / 2;
-        if(!check(y, x + h, h)) g[y + h - 1][x + h] = num; 
-        if(!check(y, x, h)) g[y + h - 1][x + h - 1] = num; 
-        if(!check(y + h, x, h)) g[y + h][x + h - 1] = num; 
-        if(!check(y + h, x + h, h)) g[y + h][x + h] = num; 
+        if(!find_hole(y, x + now, now)) tile[y + now - 1][x + now] = num;    // 1 사분면
+        if(!find_hole(y, x, now)) tile[y + now - 1][x + now - 1] = num;      // 2 사분면
+        if(!find_hole(y + now, x, now)) tile[y + now][x + now - 1] = num;    // 3 사분면
+        if(!find_hole(y + now, x + now, now)) tile[y + now][x + now] = num;  // 4 사분면
 
         num++;
 
-        calc(h, y, x);
-        calc(h, y, x + h);
-        calc(h, y+h, x);
-        calc(h, y + h, x + h);
+        lay_tiles(y, x + now, now);
+        lay_tiles(y, x, now);
+        lay_tiles(y + now, x, now);
+        lay_tiles(y + now, x + now, now);
     }
 
-    static boolean check(int y, int x, int size){
-        for (int i = y; i < y + size; i++) {
-            for (int j = x; j < x + size; j++) {
-                if(g[i][j] != 0) return true;
+    static boolean find_hole(int y, int x, int len){
+        for (int i = y; i < y + len; i++) {
+            for (int j = x; j < x + len; j++) {
+                if(tile[i][j] != 0) return true;
             }
         }
+
         return false;
     }
 
-
-    static void print_answer(){
-        for (int i = len-1; i >= 0; i--) {
-            for (int j = 0; j < len; j++) {
-                sb.append(g[i][j] + " ");
+    static void pnt(int size){
+        for (int i = size - 1; i >= 0; i--) {
+            for (int j = 0; j < size; j++) {
+                sb.append(tile[i][j] + " ");
             }
             sb.append("\n");
         }
-        System.out.println(sb);
     }
 }
