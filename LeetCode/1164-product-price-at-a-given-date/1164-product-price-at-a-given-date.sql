5a51b7a1e17977727e6a2f7c18e991c238a72774
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-20 17:08:19 +0900

    Time: 447 ms (73.07%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,21 @@
+SELECT 
+    product_id,
+    10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > DATE('2019-08-16')
+
+UNION ALL
+
+SELECT 
+    product_id,
+    new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN(
+    SELECT 
+        product_id,
+        MAX(change_date) AS date
+    FROM Products
+    WHERE change_date <= DATE('2019-08-16')
+    GROUP BY product_id
+)
