6f1e1add197a60e9bd16e41472606ece04312582
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-21 16:44:46 +0900

    [Platinum V] Title: 광부 호석, Time: 556 ms, Memory: 91460 KB, Score: 16 point -BaekjoonHub

--- 백준/Platinum/21279. 광부 호석/광부 호석.java
+++ 백준/Platinum/21279. 광부 호석/광부 호석.java
@@ -4,75 +4,77 @@ import java.util.*;
 public class Main {
     static int n, c;
 
-    static List<Jewel> arr = new ArrayList<>();
-
-    static List<Jewel>[] ax;
-
+    static List<Jewel> [] list;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        int maxy = 0;
-        int maxx = 0;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken());
-            maxy = Math.max(y, maxy);
-            int x = Integer.parseInt(st.nextToken());
-            maxx = Math.max(x, maxx);
-            int cost = Integer.parseInt(st.nextToken());
+        List<int []> jewel = new ArrayList<>();
+        int maxY = 0;
+        int maxX = 0;
 
-            arr.add(new Jewel(y, x, cost));
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            maxX = Math.max(maxY, x);
+            int y = Integer.parseInt(st.nextToken());
+            maxY = Math.max(maxX, y);
+            int c = Integer.parseInt(st.nextToken());
+
+            jewel.add(new int[] {x, y, c});
         }
 
-        ax = new List[maxx+1];
-        for (int i = 0; i < maxx+1; i++) ax[i] = new ArrayList<>();
+        list = new List[maxX+1];
+        for (int i = 0; i <= maxX; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-        for(Jewel j : arr) ax[j.x].add(j);
-        
-        long ans = 0;
-        int nowy = maxy;
+        for (int [] arr : jewel) {
+            list[arr[0]].add(new Jewel(arr[0], arr[1], arr[2]));
+        }
+
         PriorityQueue<Jewel> pq = new PriorityQueue<>((a, b) -> b.y - a.y);
+
+        long ans = 0;
         long sum = 0;
+        int curY = maxY;
+        for(int i = 0; i <= maxX; i++){
+            if(list[i].isEmpty()) continue;
 
-        for (int sx = 0; sx <= maxx; sx++) {
-            if(ax[sx].isEmpty()) continue;
-
-            for (Jewel j : ax[sx]) {
-                if(nowy >= j.y) {
+            for(Jewel j : list[i]){
+                if(j.y <= curY) {
                     pq.add(j);
                     sum += j.cost;
                 }
             }
 
-
             while(pq.size() > c){
-                int max = pq.peek().y;
-                while(!pq.isEmpty() && max == pq.peek().y){
+                int ty = pq.peek().y;
+                while(!pq.isEmpty() && pq.peek().y == ty){
                     sum -= pq.poll().cost;
                 }
-                nowy = max;
+                curY = ty;
             }
 
             ans = Math.max(ans, sum);
         }
 
-
-       System.out.println(ans);
-
+        System.out.println(ans);
     }
-
+    
     static class Jewel{
         int y;
         int x;
         int cost;
 
-        Jewel(int y, int x, int cost){
+        Jewel(int x, int y, int cost){
             this.y = y;
             this.x = x;
             this.cost = cost;
         }
     }
-
 }
