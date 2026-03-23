783829a699cedc91d9258c87121b4069ed83f13c
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-23 14:42:21 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 124 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,62 +1,57 @@
-import java.io.*;
+ import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    // 남자(1): 자기가 받은 숫자의 배수를 다 변경
-    // 여자(2): 좌우 대칭으로 변경( 번호가 중심, 펠린드롬?)
-    //
+    static int n, m;
+    static int [] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int [] arr = new int [n + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());            
         }
-        int p = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
 
-        while(p-- > 0){
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int sex = Integer.parseInt(st.nextToken());
-            int num = Integer.parseInt(st.nextToken());
 
-            if(sex == 1){
-                for (int i = num; i <= n; i += num) {
-                    arr[i] = (arr[i] == 1 ? 0 : 1);
-                    // System.out.println("["+i+"] 번 변경");
-                }
-            }
-            else{
-                int [] range = findPalindrome(n, arr, num);
-                for (int i = range[0]; i <= range[1] ; i++) {
-                    arr[i] = (arr[i] == 1 ? 0 : 1);
-                    // System.out.println("["+i+"] 번 변경");
-                }
-            }
+            int sex = Integer.parseInt(st.nextToken());
+            int idx = Integer.parseInt(st.nextToken());
+            if(sex == 1) change_boy(idx);
+            else change_girl(idx);
         }
 
-        for (int i = 1; i <= n; i += 20) {
-            for (int j = i; j < i + 20 && j <= n; j++) {
+        for (int i = 1; i <= n; i+= 20) {
+            for (int j = i; j < Math.min(i+20, n + 1); j++) {
                 System.out.print(arr[j] + " ");
             }
             System.out.println();
         }
     }
 
-    static int [] findPalindrome(int n, int [] arr, int num){
-        int [] range = new int [2];
-        int start = num;
-        int end = num;
-        while(start > 1 && end < n){
-            if(arr[start - 1] != arr[end + 1]) break;
-            start--;
-            end++;
-        }
-        range[0] = start;
-        range[1] = end;
 
-        return range;
+    static void change_boy(int idx){
+        for (int i = idx; i <= n; i += idx) {
+            arr[i] = arr[i] == 0 ? 1 : 0;
+        }
+    }
+
+    static void change_girl(int idx){
+        arr[idx] = arr[idx] == 0 ? 1 : 0;
+        for (int i = 1; i <= n; i++) {
+            int left = idx - i;
+            int right = idx + i;
+
+            if(left < 1 || right > n) break;
+
+            if(arr[left] == arr[right]){
+                arr[left] = arr[left] == 0 ? 1 : 0;
+                arr[right] = arr[left];
+            }
+            else break;
+        }
     }
 }
