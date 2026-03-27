afc3e427db98137dc9d68f91545c6e2eb10f8e2e
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-27 15:52:46 +0900

    [Gold V] Title: 수 고르기, Time: 348 ms, Memory: 27368 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,44 @@
+ import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int [] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int [n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int l = 0;
+        int r = 1;
+
+        int temp = Integer.MAX_VALUE;
+
+        while(l < n && r < n){
+            int mid = Math.abs(arr[l] - arr[r]);
+
+            if(mid < m){
+                if(r < n - 1) r++;
+                else l++;
+            }
+            else{
+                temp = Math.min(temp, mid);
+                l++;
+            }
+        }
+
+        System.out.println(temp);
+
+    }
+
+
+}
