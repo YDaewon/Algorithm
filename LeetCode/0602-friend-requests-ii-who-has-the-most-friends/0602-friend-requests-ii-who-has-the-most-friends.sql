c4206e0c66349ea14b45adf702fecdf0497a7a7d
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-28 12:57:48 +0900

    Time: 288 ms (77.37%), Space: 0B (100%) - LeetHub

--- LeetCode/0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ LeetCode/0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,29 +1,11 @@
-WITH r AS(
-    SELECT
-    requester_id AS id,
-    COUNT(accepter_id) AS cnt
-    FROM RequestAccepted
-    GROUP BY requester_id
-),
-
-a AS(
-    SELECT
-    accepter_id AS id,
-    COUNT(requester_id) AS cnt
-    FROM RequestAccepted
-    GROUP BY accepter_id
-),
-
-u AS(
-    SELECT * FROM a
-    UNION ALL
-    SELECT * FROM r
-)
-
 SELECT 
-    id,
-    SUM(cnt) AS num
-FROM u
-GROUP BY ID
+    requester_id AS id,
+    COUNT(*) AS num
+FROM (
+SELECT requester_id FROM RequestAccepted
+UNION ALL
+SELECT accepter_id FROM RequestAccepted
+) a
+GROUP BY id
 ORDER BY num desc
 LIMIT 1
