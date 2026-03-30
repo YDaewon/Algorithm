fe4983239c8f3b1ca47bc2d155d17206d1c2b38c
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-30 11:56:37 +0900

    [Silver III] Title: 음유시인 영재, Time: 144 ms, Memory: 19452 KB -BaekjoonHub

--- 백준/Silver/19948. 음유시인 영재/음유시인 영재.java
+++ 백준/Silver/19948. 음유시인 영재/음유시인 영재.java
@@ -1,51 +1,58 @@
-import java.io.*;
+ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char [] str = br.readLine().toCharArray();
+
         int space = Integer.parseInt(br.readLine());
-        int [] alpha = new int [26];
-        StringTokenizer st = new StringTokenizer((br.readLine()));
-        
+
+        int [] alpha = new int[26];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 26; i++) {
             alpha[i] = Integer.parseInt(st.nextToken());
         }
-        String answer = "";
-        answer += (str[0] >= 'A' && str[0] <= 'Z') ? str[0] : (char)(str[0] - 32);
-        int i = 0;
-        while(i < str.length){
-            if((str[i] >= 'A' && str[i] <= 'Z') && alpha[str[i] - 'A'] > 0){
-                alpha[str[i] - 'A']--;
-                int j = i + 1;
-                while(j < str.length && str[i] == str[j]) j++;
-                i = j - 1;
-            } else if((str[i] >= 'a' && str[i] <= 'z') && alpha[str[i] - 'a'] > 0){
-                alpha[str[i] - 'a']--;
-                int j = i + 1;
-                while(j < str.length && str[i] == str[j]) j++;
-                i = j - 1;
-            } else if(str[i] == ' ' && space > 0){
+
+        int idx = 0;
+        String title = "";
+        if(str[0] >= 'a') title += (char)(str[0] - 'a' + 'A');
+        else title += str[0];
+
+        while(idx < str.length) {
+            char now = str[idx];
+            if(now >= 'a' && now <= 'z' && alpha[now - 'a'] > 0){
+                alpha[now - 'a']--;
+                while(idx < str.length && now == str[idx]) idx++;
+            }
+            else if(now == ' ' && space > 0){
                 space--;
-                while(i < str.length && str[i] == ' ') i++;
-                answer += (str[i] >= 'A' && str[i] <= 'Z') ? str[i] : (char)(str[i] - 32);
-            } else{
+                while(idx < str.length && now == str[idx]) idx++;
+                
+                if(str[idx] >= 'a') title += (char)(str[idx] - 'a' + 'A');
+                else title += str[idx];
+            }
+            else if(now >= 'A' && now <= 'Z' && alpha[now - 'A'] > 0){
+                alpha[now - 'A']--;
+                while(idx < str.length && now == str[idx]) idx++;
+            }
+            else{
                 break;
             }
-           //System.out.print(str[i]);
-            i++;
         }
 
-        for (int j = 0; j < answer.length(); j++) {
-            char c = answer.charAt(j);
+        for (int j = 0; j < title.length(); j++) {
+            char c = title.charAt(j);
             if(alpha[c - 'A'] <= 0) {
-                i = -1;
+                idx = -1;
                 break;
             }
         }
-        if(i == str.length) System.out.println(answer);
-        else System.out.println(-1);
 
+        System.out.println(idx == str.length ? title : - 1);
     }
+
+
 }
