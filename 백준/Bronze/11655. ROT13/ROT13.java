696f5519e21eb4137a3df638aa1f26ae525a3387
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-03-31 12:12:40 +0900

    [Bronze I] Title: ROT13, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -0,0 +1,31 @@
+ import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int [][] board;
+    static boolean [][] visit;
+    static int ans = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str = br.readLine().toCharArray();
+
+        for (int i = 0; i < str.length; i++) {
+            int t = str[i];
+            if(t >= 'A' && t <= 'Z'){
+                t = t - 'A' + 13;
+                t %= 26;
+                str[i] = (char)(t + 'A');
+            }
+            else if(t >= 'a' && t <= 'z'){
+                t = t - 'a' + 13;
+                t %= 26;
+                str[i] = (char)(t + 'a');
+            }
+        }
+
+        for (int i = 0; i < str.length; i++) {
+            System.out.print(str[i]);
+        }
+    }
+}
