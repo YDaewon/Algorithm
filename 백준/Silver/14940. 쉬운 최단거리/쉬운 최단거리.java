dce1c5cc6f4cc61a675314037cd7e53be08dc9ba
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-03 11:58:37 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1744 ms, Memory: 77172 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int [][] map;
+    static int [][] cnt;
+
+    static int [] dy = {-1,1,0,0};
+    static int [] dx = {0,0,-1,1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int [n][m];
+        cnt = new int [n][m];
+        int y = 0;
+        int x = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            Arrays.fill(cnt[i], Integer.MAX_VALUE);
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 2){
+                    y = i;
+                    x = j;
+                }
+            }
+        }
+
+        bfs(y, x);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(cnt[i][j] == Integer.MAX_VALUE){
+                    if(map[i][j] == 0) System.out.print(0 + " ");
+                    else System.out.print(-1 + " ");
+                }
+                else System.out.print(cnt[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    private static void bfs(int y, int x) {
+        Queue<int []> q = new LinkedList<>();
+        cnt[y][x] = 0;
+        q.add(new int [] {y, x, cnt[y][x]});
+
+        while(!q.isEmpty()){
+            int [] c = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = c[0] + dy[i];
+                int nx = c[1] + dx[i];
+
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+                if(map[ny][nx] == 1 && cnt[ny][nx] > c[2] + 1){
+                    cnt[ny][nx] = c[2] + 1;
+                    q.add(new int [] {ny,nx, c[2] + 1});
+                }
+            }
+        }
+    }
+
+}
