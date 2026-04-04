3c36a9734ea515b62336cac6792c814e34144993
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-04 13:09:39 +0900

    Time: 335 ms (71.87%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,9 @@
+SELECT
+    c1.visited_on,
+    SUM(c2.amount) AS amount,
+    ROUND(AVG(c2.amount), 2) AS average_amount
+FROM (SELECT visited_on, SUM(amount) AS amount FROM Customer GROUP BY visited_on) c1
+JOIN (SELECT visited_on, SUM(amount) AS amount FROM Customer GROUP BY visited_on) c2 
+ON DATEDIFF(c1.visited_on, c2.visited_on) >= 0 AND DATEDIFF(c1.visited_on, c2.visited_on) <= 6
+GROUP BY c1.visited_on
+HAVING COUNT(c2.amount) = 7
