19744162f30e6642cade6233769cdf3e2bc79953
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-05 15:36:08 +0900

    Time: 285 ms (26.63%), Space: 0B (100%) - LeetHub

--- LeetCode/0176-second-highest-salary/0176-second-highest-salary.sql
+++ LeetCode/0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,6 @@
+# Write your MySQL query statement below
 SELECT
-    MAX(salary) AS SecondHighestSalary
+    (CASE WHEN COUNT(DISTINCT salary) > 0 THEN MAX(salary) END) AS SecondHighestSalary
 FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee);
+WHERE salary NOT IN (SELECT MAX(salary) FROM Employee)
+ORDER BY salary DESC
