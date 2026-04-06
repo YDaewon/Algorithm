c37ce7a1e01d47dd4946c6af637f76fbad6f209c
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-06 13:05:54 +0900

    [Silver I] Title: 샤워실 바닥 깔기 (Small), Time: 108 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/14600. 샤워실 바닥 깔기 （Small）/샤워실 바닥 깔기 （Small）.java
+++ 백준/Silver/14600. 샤워실 바닥 깔기 （Small）/샤워실 바닥 깔기 （Small）.java
@@ -2,59 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int k;
-    
-    static int num = 1;
-    static int [][] board;
+    static int n;
+    static int [][] arr;
+
+    static int cnt = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        k = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int x = Integer.parseInt(st.nextToken()) - 1;
-        int y = Integer.parseInt(st.nextToken()) - 1;
-        int len = (int)Math.pow(2, k);
-        board = new int [len][len];
-        board[y][x] = -1;
+        n = Integer.parseInt(st.nextToken());
+        int len = (int) Math.pow(2, n);
+        arr = new int [len][len];
+        st = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st.nextToken())-1;
+        int x = Integer.parseInt(st.nextToken())-1;
 
-        go(0,0, len);
+        arr[x][y] = -1;
+
+        go(len, 0, 0);
 
         StringBuilder sb = new StringBuilder();
 
         for (int i = len - 1; i >= 0; i--) {
             for (int j = 0; j < len; j++) {
-                sb.append(board[i][j] + " ");
+                sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
         }
+
         System.out.println(sb);
     }
 
-    static void go(int y, int x, int l){
-        if(l == 1) return;
+    static void go(int len, int y, int x){
+        if(len == 1) return;
+        int h = len / 2;
+        if(find(h, y, x)) arr[y + h - 1][x + h - 1] = cnt;
+        if(find(h, y, x + h)) arr[y + h - 1][x + h] = cnt;
+        if(find(h, y + h, x)) arr[y + h][x + h - 1] = cnt;
+        if(find(h, y + h, x + h)) arr[y + h][x + h] = cnt;
 
-        int half = l / 2;
+        cnt++;
 
-        if(!find(y, x, half)) board[y + half - 1][x + half - 1] = num;
-        if(!find(y, x + half, half)) board[y + half - 1][x + half] = num;
-        if(!find(y + half, x, half)) board[y + half][x + half - 1] = num;
-        if(!find(y + half, x + half, half)) board[y + half][x + half] = num;
-        num++;
-
-        go(y, x, half);
-        go(y, x + half, half);
-        go(y + half, x, half);
-        go(y + half, x + half, half);
+        go(h, y, x);
+        go(h, y, x + h);
+        go(h, y + h, x);
+        go(h, y + h, x + h);
     }
 
-
-    static boolean find(int y, int x, int l){
-        for (int i = y; i < y + l; i++) {
-            for (int j = x; j < x + l; j++) {
-                if(board[i][j] != 0) return true;
+    private static boolean find(int h, int y, int x) {
+        for (int i = y; i < y + h; i++) {
+            for (int j = x; j < x + h; j++) {
+                if(arr[i][j] != 0) return false;
             }
         }
-        return false;
+
+        return true;
     }
+
+
 }
