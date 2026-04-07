abf3fb8f1e36146f28b3d3b54283d59197672c61
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-07 13:08:18 +0900

    [Silver II] Title: 자원 캐기, Time: 196 ms, Memory: 22636 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int [][] arr = new int [n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int [][] dp = new int [n+1][m+1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + arr[i][j];
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+}
