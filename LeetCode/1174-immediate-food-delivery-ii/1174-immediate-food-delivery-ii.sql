caf43157d10dda279ec2fa200b9064aac8bc4dcd
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-09 14:04:28 +0900

    Time: 676 ms (24.07%), Space: 0B (100%) - LeetHub

--- LeetCode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ LeetCode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -8,7 +8,7 @@ WITH A AS(
 )
 
 SELECT
-    ROUND(100 * SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) / COUNT(*) , 2) AS immediate_percentage
+    ROUND(100 * AVG(order_date = customer_pref_delivery_date), 2) AS immediate_percentage
 FROM Delivery
 WHERE (customer_id, order_date) IN (SELECT * FROM A)
 
