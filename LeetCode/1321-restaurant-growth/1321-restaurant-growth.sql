84cf2a32680996066c18f5a64fc8fee38f431042
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-11 14:06:39 +0900

    Time: 360 ms (34.73%), Space: 0B (100%) - LeetHub

--- LeetCode/1321-restaurant-growth/1321-restaurant-growth.sql
+++ LeetCode/1321-restaurant-growth/1321-restaurant-growth.sql
@@ -3,7 +3,7 @@ SELECT
     SUM(c2.amount) AS amount,
     ROUND(AVG(c2.amount), 2) AS average_amount
 FROM (SELECT visited_on, SUM(amount) AS amount FROM Customer GROUP BY visited_on) c1
-JOIN (SELECT visited_on, SUM(amount) AS amount FROM Customer GROUP BY visited_on) c2 
+JOIN (SELECT visited_on, SUM(amount) AS amount FROM Customer GROUP BY visited_on) c2
 ON DATEDIFF(c1.visited_on, c2.visited_on) >= 0 AND DATEDIFF(c1.visited_on, c2.visited_on) <= 6
 GROUP BY c1.visited_on
-HAVING COUNT(c2.amount) = 7
+HAVING COUNT(c2.visited_on) = 7
