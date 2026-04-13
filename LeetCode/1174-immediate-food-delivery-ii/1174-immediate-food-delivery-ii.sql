d442f5448591da6442024e8fa4d3e93fd8bf8c68
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-13 15:35:44 +0900

    Time: 676 ms (24.05%), Space: 0B (100%) - LeetHub

--- LeetCode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ LeetCode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,12 +1,13 @@
-WITH A AS(
-    SELECT
-        customer_id,
-        MIN(order_date) AS first_order
-    FROM Delivery
-    GROUP BY customer_id
-)
+
+
 
 SELECT
-    ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS immediate_percentage
+    ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) AS immediate_percentage
 FROM Delivery
-WHERE (customer_id, order_date) IN (SELECT * FROM A)
+WHERE (customer_id, order_date) IN ( 
+    SELECT
+    customer_id,
+    MIN(order_date)
+FROM Delivery
+GROUP BY customer_id
+)
