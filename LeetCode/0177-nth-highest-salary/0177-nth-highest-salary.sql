f60099a31bb3d3c8a4a2b2ca901fe9950e89fc4a
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-04-15 15:02:55 +0900

    Time: 431 ms (50.52%), Space: 0B (100%) - LeetHub

--- LeetCode/0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ LeetCode/0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,12 +1,8 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-      # Write your MySQL query statement below.
-      SELECT (CASE WHEN COUNT(salary) >= N AND N > 0 THEN MIN(salary) END) FROM 
-      (SELECT
-        DISTINCT salary
-      FROM Employee
-      ORDER BY salary DESC
-      LIMIT N) AS t
+    SELECT 
+        CASE WHEN COUNT(*) >= N AND N> 0 THEN MIN(salary) END  
+    FROM (SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT N) AS t
   );
 END
